7031d70d8df99d97081c988c47ee5295814e0ff3
toluthedev <enochjesse884@gmail.com>
2024-01-08 17:30:42 +0100

    Add createUser and getDetails functions to db.interact.js

--- utils/db.interact.js
+++ utils/db.interact.js
@@ -1,2 +1,26 @@
 const User = require('../database/model/user.model');
 
+//create user db function to call in the command section
+const createUser = async (user) => {
+    try {
+        const newUser = new User(user);
+        await newUser.save();
+        return newUser;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const getDetails = async (user) => {
+    try {
+        const userDetails = await User.findOne({ name: user });
+        return userDetails;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+module.exports = {
+    createUser,
+    getDetails
+}
